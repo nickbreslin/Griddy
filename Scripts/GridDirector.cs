74e19a3098ac2b34a23acd0b3ce63d21a0620041
nickbreslin <nickbreslin@gmail.com>
2012-05-02 00:00:57 -0500

    Purpose: Removing Test Values from Director

--- Scripts/GridDirector.cs
+++ Scripts/GridDirector.cs
@@ -5,13 +5,13 @@ public class GridDirector : MonoSingleton<GridDirector> {
 	
 	void Start()
 	{
-		GridViewController.instance.AddView("test", ViewDirector);
-		GridViewController.instance.SetView("fake");
+		//GridViewController.instance.AddView("test", ViewDirector);
+		//GridViewController.instance.SetView("fake");
 		//GridViewController.instance.SetView("test");
 	}
 	
 	void ViewDirector()
 	{
-		GUILayout.Label("Moo");
+		GUILayout.Label("Director");
 	}
 }
