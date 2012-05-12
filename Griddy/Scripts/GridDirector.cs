65225bbc9078f3f5e710ae94c78171e8598fea67
nickbreslin <nickbreslin@gmail.com>
2012-05-11 22:29:58 -0500

    Purpose: Sterilized GridDirector

--- Griddy/Scripts/GridDirector.cs
+++ Griddy/Scripts/GridDirector.cs
@@ -4,20 +4,4 @@ using System.Collections;
 public class GridDirector : MonoSingleton<GridDirector>
 {
 	public bool isDebugMode;
-	
-	void Start()
-	{
-	}
-	
-	void OnGUI()
-	{
-		if(GUILayout.Button ("Generate Square Grid"))
-		{
-			GridGenerator.instance.Generate(Vector3.zero, 5, 5);
-		}
-		if(GUILayout.Button ("Generate Hex Grid"))
-		{
-			GridGenerator.instance.GenerateHex(new Vector3(0,0,10), 3, 3);
-		}
-	}
 }
