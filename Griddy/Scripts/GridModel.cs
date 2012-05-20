709af55a14f9971403c8077338f9d214fdaf7a73
nickbreslin <nickbreslin@gmail.com>
2012-05-19 20:21:16 -0500

    Purpose: Grid Model

---
+++ Griddy/Scripts/GridModel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+/**
+ * GridController
+ *
+ */
+public class GridModel : MonoBehaviour
+{
+    public void StartHover()
+    {
+    }
+    
+    public void StopHover()
+    {
+    }
+    
+    public void OnRightClick()
+    {
+    }
+    
+    public void OnLeftClick()
+    {
+    }
+}
